5c6a00493f6d9f890068f222e1a1641b10e52dae
ash-veil <thyra.veil@gmail.com>
2025-09-15 17:48:33 +0100

    updated associations

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,9 +1,21 @@
-import {Sequelize} from 'sequelize'
+import { Sequelize } from "sequelize";
+
+import userModel from "../model/User.js";
+import projectModel from "../model/Project.js";
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-    logging: false,
-})
+  dialect: "sqlite",
+  storage: "./database.sqlite",
+  logging: false,
+});
 
-export default sequelize;
+const User = userModel(sequelize);
+const Project = projectModel(sequelize);
+
+User.hasMany(Project, {foreignKey: "owner_id"})
+Project.belongsTo(User, {as: "owner", foreignKey: "owner_id"})
+
+User.belongsToMany(Project, { through: "collaboration" });
+Project.belongsToMany(User, { through: "collaboration" });
+
+export  { sequelize, User, Project };
