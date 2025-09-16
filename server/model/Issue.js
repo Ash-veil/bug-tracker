2a4a996c1e67a52286aaf9bfa4d701f7a640e986
ash-veil <thyra.veil@gmail.com>
2025-09-16 13:37:38 +0100

    added issue model and collabs

---
+++ server/model/Issue.js
@@ -0,0 +1,29 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  const Issue = sequelize.define("issue", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    status: {
+      type: DataTypes.STRING,
+    },
+    priority: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+
+  return Issue;
+};
